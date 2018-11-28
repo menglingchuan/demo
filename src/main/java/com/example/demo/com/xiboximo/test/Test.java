b244911b5d0298b23385f9993a03a41b57865df6
chuanchuan.meng <1152363990@qq.com>
2018-11-28 09:38:53 +0800

    试试le

---
+++ src/main/java/com/example/demo/com/xiboximo/test/Test.java
@@ -0,0 +1,9 @@
+package com.example.demo.com.xiboximo.test;
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("hehehe");
+        System.out.println("haha");
+    }
+
+}
